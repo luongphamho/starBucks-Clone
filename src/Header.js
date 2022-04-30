dfd8d9e5e1666fdac7bd8b8e19b1c01d111e1862
luongphamho <phamholuong@gmail.com>
2022-04-30 16:32:35 +0700

    first commit 108 error

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Example } from "./Example";
+import FindAStore from "./FindAStore";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Link className="header__logo" to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
+            alt=""
+          />
+        </Link>
+        <Link to="/menu" className="header__link">
+          Menu
+        </Link>
+        <Link className="header__link">Rewards</Link>
+        <Link className="header__link">Gift Cards</Link>
+      </div>
+      <div className="header__right">
+          <Example/>
+          <FindAStore/>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HomeScreen.css'
+function HomeScreen() {
+  return (
+    <div>HomeScreen</div>
+  )
+}
+
+export default HomeScreen
