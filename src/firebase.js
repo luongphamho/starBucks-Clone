e328ef8e3defec2fba36c77ea359b9f0897390ef
luongphamho <phamholuong@gmail.com>
2022-05-01 23:42:45 +0700

    final change dist

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAv6JnVO2xMtTTF6PG_IBwWYECkPJWpzOs",
-    authDomain: "starbuck-clone-acb05.firebaseapp.com",
-    projectId: "starbuck-clone-acb05",
-    storageBucket: "starbuck-clone-acb05.appspot.com",
-    messagingSenderId: "987433352510",
-    appId: "1:987433352510:web:bb74d5a8ab789a36f4406f"
-  };
+  apiKey: "AIzaSyAv6JnVO2xMtTTF6PG_IBwWYECkPJWpzOs",
+  authDomain: "starbuck-clone-acb05.firebaseapp.com",
+  projectId: "starbuck-clone-acb05",
+  storageBucket: "starbuck-clone-acb05.appspot.com",
+  messagingSenderId: "987433352510",
+  appId: "1:987433352510:web:bb74d5a8ab789a36f4406f"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
