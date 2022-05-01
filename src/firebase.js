fff956815818ae7dd2b9cdda728d61df31fb653f
luongphamho <phamholuong@gmail.com>
2022-05-01 23:54:06 +0700

    final build and deploy firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAv6JnVO2xMtTTF6PG_IBwWYECkPJWpzOs",
@@ -6,11 +6,11 @@ const firebaseConfig = {
   projectId: "starbuck-clone-acb05",
   storageBucket: "starbuck-clone-acb05.appspot.com",
   messagingSenderId: "987433352510",
-  appId: "1:987433352510:web:bb74d5a8ab789a36f4406f"
+  appId: "1:987433352510:web:bb74d5a8ab789a36f4406f",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const auth = firebaseApp.auth()
+const auth = firebaseApp.auth();
 
-export { auth }
+export { auth };
